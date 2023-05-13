431930bde168d7afe18d4f69e6498f98e47f487b
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2023-05-13 11:07:03 +0700

    Add custom tailwind properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        primary: "#00668A",
+        secondary: "#004E71",
+      },
+    },
+    fontFamily: {
+      roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
   },
   plugins: [],
 };
